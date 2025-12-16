dd1127746378e10bc2df206296404c0e4d438472
Ovs9nka228 <amirgalimullin45@gmail.com>
2025-12-16 16:39:12 +0500

    Delete ОАИП/3 deadline/Лабораторная работа №1 directory

--- ОАИП/3 deadline/Лабораторная работа №1/2z.py
+++
@@ -1,8 +0,0 @@
-def make_bold(func):
-    def wrapper():
-        result = func()
-        return f"<b>{result}</b>"
-    return wrapper
-def get_text():
-    return "Hello, World!"
-print(get_text())
